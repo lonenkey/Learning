14832922c4695c7e690e196ca422d89534cd2caa
LonEnkey <lonenkey@gmail.com>
2018-01-21 21:22:42 -0600

    Its alive.  It works

--- showrun.py
+++ showrun.py
@@ -1,5 +1,7 @@
 #python 3.5.2
 import pexpect
+import sys
+import os
 
 device_list = list()
 device_info = {}
@@ -10,8 +12,12 @@ for device in file:
     username = device_info[1]
     password = device_info[2]
     enable   = device_info[3]
+    hostname = device_info[4]
+    hostname = hostname.strip()
 ##  connect session and send commands
     session = pexpect.spawn('telnet ' + ip, timeout=20)
+    session.maxread=9999999
+    #session.logfile = sys.stdout
     result = session.expect(['Username:', pexpect.TIMEOUT])
     if result != 0:
         print ("fail on ", ip)
@@ -26,28 +32,23 @@ for device in file:
     if result != 0:
         print ("fail on ", ip)
         exit()
-    print ("You're clear kid.  Now blow this thing and lets go home.")
     session.sendline('enable')
-    result = session.expect(['Password:'])
+    result = session.expect(['Password:', pexpect.TIMEOUT])
     if result != 0:
          print ("fail on ", ip)
          exit()
     session.sendline(enable)
+    result = session.expect(['#', pexpect.TIMEOUT])
     if result != 0:
         print ("fail on ", ip)
         exit()
-    print('show ip completed')
-    
     session.sendline('sh run | i hostname')
-    result = session.expect
-    if result == 0:
-        print ('Command sent ok')
-    elif result == 1:
-        print ('Timed out.')
-    elif result == 2:
-        print ('EOF')
-    show_output = session.before
+    result = session.expect([hostname+'#', pexpect.TIMEOUT])
+    if result != 0:
+        print ("fail on ", ip)
+        print (hostname+'#')
+        exit()
+    output = session.before
+    print(output.decode('utf-8'))
     session.close
-##  gather command output and save somehow
-    device_list.append(device_info[0])
-    print ('Router ', show_output)
+file.close()
